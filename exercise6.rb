4ae859d03cb3500dfd132464f2d2414d12571a5c
Gowri Nambiar <gowri.nambi@gmail.com>
2013-10-22 18:00:31 -0400

    grocery_list sort&delete

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,30 @@
+
+
 grocery_list = ["eggs", "milk", "fish", "apples"]
-grocery_list.each { |food| puts "* " + food}
+
+def add_a (list)
+	list.each { |food| puts "* " + food}
+end
+
+grocery_list << "rice"
+
+add_a(grocery_list)
+
+grocery_list.length
+
+
+if grocery_list.include? "bananas"
+	puts "You don't need to pick up bananas"
+else
+	puts "You need to pick up bananas"
+end
+
+grocery_list[1]
+
+grocery_list.sort
+
+add_a(grocery_list)
+
+grocery_list.delete_at(2)
+
+add_a(grocery_list)
